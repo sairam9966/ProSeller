fcf18d0048ba2a2a06d9967ce5cf1057f7db701d
SaiRam Gudimetla <sairamgudimetla953@gmail.com>
2023-07-16 00:18:04 +0530

    updating

---
+++ backend/models/productModel.js
@@ -0,0 +1,75 @@
+import { defaultMaxListeners } from "events";
+import mongoose from "mongoose";
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const productScehma = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("product", productScehma);
+
+export default Product;
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import productRoutes from "./routes/productRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+
+const port = process.env.PORT || 5000;
+connectDB();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello api running...");
+});
+app.use("/api/products", productRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
