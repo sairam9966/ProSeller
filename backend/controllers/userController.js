007541f1b91234c162bad0142b51083c5cdad4f3
SaiRam Gudimetla <sairamgudimetla953@gmail.com>
2023-07-18 13:59:21 +0530

    updating

---
+++ backend/controllers/userController.js
@@ -0,0 +1,147 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+//@desc Auth user&get token
+//@route POST /api/users/login
+//@access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+//@desc Register user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+//@desc Logout user/clear cookie
+//route POST /api/users/logout
+//@access private
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+  res.status(200).json({ message: "Logged out successfully" });
+});
+
+//@desc Get user profile
+//route GET /api/users/profile
+//@access private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not found");
+  }
+});
+
+//@desc update user profile
+//route PUT /api/users/profile
+//@access private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.staus(404);
+    throw new Error("User Not Found");
+  }
+});
+
+//@desc Get users
+//route GET /api/users
+//@access private/admin
+
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
+});
+
+//@desc Get user by id
+//route GET /api/users/:id
+//@access private/admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get user by id");
+});
+
+//@desc Delete users
+//route DELETE /api/users/:id
+//@access private/admin
+
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user");
+});
+
+//@desc update user
+//route PUT /api/users/:id
+//@access private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update user");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
@@ -8,11 +9,16 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
-
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//cookie parser middleware
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("hello api running...");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {
