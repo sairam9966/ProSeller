b1ad9655e10e838c4f5af470d6cd052da0b4c078
SaiRam Gudimetla <sairamgudimetla953@gmail.com>
2023-07-26 17:21:47 +0530

    updating

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const __dirname = path.resolve(); //set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/uploads")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
