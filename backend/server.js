e05cbf9c13dd980ffe8745ee12fd7bcc21343e0e
SaiRam Gudimetla <sairamgudimetla953@gmail.com>
2023-07-26 13:33:27 +0530

    updating

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("hello api running...");
-});
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -29,6 +27,18 @@ app.get("/api/config/paypal", (req, res) =>
 );
 const __dirname = path.resolve(); //set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/uploads")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {
